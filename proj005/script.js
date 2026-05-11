9c53f2404d4eff8489b3bc06657e9f581196ddbc
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-10 21:38:07 -0300

    proj005 adicionado

--- proj005/script.js
+++ proj005/script.js
@@ -1,18 +1,18 @@
 const btn = document.getElementById('btn-mobile')
 const menu = document.getElementById('menu')
-const icon = document.getElementById('icon')
+const links = document.querySelectorAll('menu-link')
+
 
 btn.addEventListener('click', () => {
 
     menu.classList.toggle('active')
 
     if (menu.classList.contains('active')) {
-
-        icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>'
-
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>'
     } else {
-
-        icon.innerHTML = '<svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>'
-
+        btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>`
     }
+
+    
+
 })
