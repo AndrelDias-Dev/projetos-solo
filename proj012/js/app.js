e545fea285e887254c206a01ae37ee038f77a53c
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-08 01:44:00 -0300

    proj012 adicionado

--- proj012/js/app.js
+++ proj012/js/app.js
@@ -1,9 +1,12 @@
 import { movies } from "./movies.js";
-import { renderSlide, initSlider } from "./slider.js";
+import { renderSlide, initSlider, } from "./slider.js";
+import { initSlider2 } from "./slider2.js";
 
 renderSlide(movies);
 
 initSlider()
+
+initSlider2()
 
 
 
--- proj012/js/slider2.js
+++ proj012/js/slider2.js
@@ -1,75 +1,70 @@
-const wrapper = document.querySelector('.swiper-wrapper')
+import { movies } from "./movies.js";
 
-fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies')
-    .then(response => response.json())
-    .then(dados => {
+export function initSlider2() {
+    const wrapper = document.querySelector('.swiper-wrapper');
 
-        const filmes = dados.map(filme => {
-
-            return {
-                rating: filme.Runtime,
-                titulo: filme.Title,
-                ano: filme.Year,
-                poster: filme.Poster
-            }
-        })
-
-        wrapper.innerHTML = filmes.map(filme => {
-
-    return `
-            <div class="swiper-slide">
-                <article class="movie-card">
-                    <img src="${filme.poster}" alt="${filme.titulo}">
-                    <div class="rating">
-                        <i class="fa-solid fa-star"></i>
-                        ${filme.rating}
-                    </div>
-                    <div class="icons">
-                        <i class="fa-solid fa-circle-info"></i>
-                        <i class="fa-solid fa-plus"></i>
-                    </div>
-                    <div class="description">
-                        <h2>${filme.titulo}</h2>
-                        <span class="year">
-                            <p>${filme.ano}</p>
-                        </span>
-                    </div>
-                </article>
-            </div>
-        `
-}).join('')
-    })
-
+wrapper.innerHTML = movies.map(filme => {
+  return `
+    <div class="swiper-slide">
+      <article class="movie-card">
+        <img src="${filme.image}" alt="${filme.title}">
+        <div class="rating">
+          <i class="fa-solid fa-star"></i>
+          ${filme.stars}
+        </div>
+        <div class="bottom-card">
+        <div class="icons">
+          <i class="fa-solid fa-circle-info"></i>
+          <i class="fa-solid fa-plus"></i>
+        </div>
+        <div class="description">
+          <h2>${filme.title}</h2>
+          <span class="year">
+            <p>${filme.year}</p>
+          </span>
+        </div>
+        </div>
+      </article>
+    </div>
+  `;
+}).join('');
 
 const swiper = new Swiper('.filmesSwiper', {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  centeredSlides: false,
+  speed: 300,
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: { slidesPerView: 3 },
+    1200: { slidesPerView: 5 }
+  },
 
-    slidesPerView:3,
-
-    spaceBetween:20,
-
-    speed:300,
-
-    centeredSlides:true,
-
-    loop:true,
-
-    grabCursor: true,
-
-    autoplay:{
-        delay: 1500,
-        pauseOnMouseEnter:true
-    },
-
-    breakpoints:{
-
-        768:{
-            slidesPerView:3
-        },
-
-        1200:{
-            slidesPerView:5
-        }
-
-    }
-
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
+
+swiper.el.addEventListener('mouseenter', () => {
+  swiper.autoplay.stop();
+});
+
+
+swiper.el.addEventListener('mouseleave', () => {
+  swiper.autoplay.start();
+});
+}
+
+
+
+
+
+
+
+
