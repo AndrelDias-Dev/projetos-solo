ad9859c5804c1ecc6c6960f02d8b2183101c5d2b
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-11 11:31:44 -0300

    proj013 adicionado

--- proj013/js/dark-theme.js
+++
@@ -1,6 +0,0 @@
-const btnTheme = document.querySelector('.btn-theme');
-const body = document.querySelector('body')
-
-btnTheme.addEventListener('click', () => {
-    body.classList.toggle('dark-theme')
-})
