efdc79f774e2d16ca49d37d8c148920d3d391896
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-11 17:19:10 -0300

    proj015 adicionado

---
+++ proj015/js/app.js
@@ -0,0 +1,6 @@
+import { renderProducts } from "./render.js";
+import { movies } from "./movies.js";
+
+
+renderProducts(movies)
+
---
+++ proj015/js/movies.js
@@ -0,0 +1,43 @@
+export const movies = [
+    {
+        id:1, img:'assets/imagens/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:2, img:'assets/imagens/horror.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:3, img:'assets/imagens/astronaut.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:4, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:5, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:6, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:7, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:8, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:9, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    {
+        id:10, img:'assets/action.jpg', title:'Uncharted: O Mapa Perdido', year: 2012, hours: '2h 40min', genre: 'Ação / Aventura', rotten: '83%', stars:8.4, info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cum neque sunt ut illo, voluptate'
+    },
+
+    
+]
