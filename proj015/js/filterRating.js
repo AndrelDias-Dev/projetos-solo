09e713baef75ccb573ef99967fd540bf80b9c241
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-16 00:34:05 -0300

    proj015 adicionado

--- proj015/js/filterRating.js
+++
@@ -1,8 +0,0 @@
-export function filterRating(movies, rating) {
-
-    if (rating === '') {
-        return movies
-    }
-
-    return movies.filter(movie => movie.stars >= Number(rating))
-}
--- proj015/movies.js
+++ proj015/movies.js
@@ -19,7 +19,7 @@ export const movies = [
         hours: '1h 43min',
         genre: ['Terror', 'Horror'],
         rotten: '88%',
-        stars: 7.8,
+        stars: 8.1,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
@@ -28,94 +28,94 @@ export const movies = [
         img: 'assets/imagens/astronaut.jpg',
         title: 'A Órbita do Espaço',
         year: 2024,
-        hours: '2h 15min',
+        hours: '2h 10min',
         genre: ['Ficção Científica', 'Aventura'],
         rotten: '91%',
-        stars: 9.1,
+        stars: 9.2,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 4,
         img: 'assets/imagens/wars.jpg',
-        title: 'Guerras do Amanhã',
+        title: 'Guerras do Destino',
         year: 2018,
-        hours: '2h 20min',
-        genre: ['Ação', 'Ficção Científica'],
+        hours: '2h 25min',
+        genre: ['Ação', 'Aventura'],
         rotten: '79%',
-        stars: 7.2,
+        stars: 7.6,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 5,
         img: 'assets/imagens/yakuza.jpg',
-        title: 'Yakuza',
-        year: 2008,
-        hours: '1h 55min',
+        title: 'Yakuza: Código de Honra',
+        year: 2022,
+        hours: '2h 05min',
         genre: ['Ação', 'Crime'],
-        rotten: '74%',
-        stars: 6.9,
+        rotten: '86%',
+        stars: 8.7,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 6,
         img: 'assets/imagens/action.jpg',
-        title: 'Operação Extrema',
-        year: 2023,
-        hours: '2h 05min',
-        genre: ['Ação', 'Thriller'],
-        rotten: '86%',
-        stars: 8.3,
+        title: 'Operação Impossível',
+        year: 2015,
+        hours: '1h 58min',
+        genre: ['Ação', 'Crime'],
+        rotten: '74%',
+        stars: 7.2,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 7,
         img: 'assets/imagens/action.jpg',
-        title: 'O Último Combate',
-        year: 2015,
-        hours: '1h 48min',
+        title: 'O Último Soldado',
+        year: 2020,
+        hours: '2h 15min',
         genre: ['Ação', 'Drama'],
-        rotten: '72%',
-        stars: 7.1,
+        rotten: '89%',
+        stars: 8.9,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 8,
         img: 'assets/imagens/action.jpg',
-        title: 'A Grande Aventura',
-        year: 2020,
-        hours: '2h 30min',
-        genre: ['Aventura', 'Comédia'],
-        rotten: '82%',
-        stars: 8.0,
+        title: 'Além do Horizonte',
+        year: 2017,
+        hours: '2h 02min',
+        genre: ['Aventura', 'Drama'],
+        rotten: '81%',
+        stars: 7.9,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 9,
         img: 'assets/imagens/action.jpg',
-        title: 'Riso Fatal',
-        year: 2011,
-        hours: '1h 35min',
-        genre: ['Comédia', 'Terror'],
-        rotten: '68%',
-        stars: 6.5,
+        title: 'Código Vermelho',
+        year: 2025,
+        hours: '2h 18min',
+        genre: ['Ação', 'Suspense'],
+        rotten: '94%',
+        stars: 9.5,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     },
 
     {
         id: 10,
         img: 'assets/imagens/action.jpg',
-        title: 'Horizonte Perdido',
-        year: 2025,
-        hours: '2h 50min',
-        genre: ['Aventura', 'Ficção Científica'],
-        rotten: '94%',
-        stars: 9.4,
+        title: 'A Última Jornada',
+        year: 2019,
+        hours: '1h 52min',
+        genre: ['Drama', 'Aventura'],
+        rotten: '77%',
+        stars: 7.4,
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
     }
-]
+];
