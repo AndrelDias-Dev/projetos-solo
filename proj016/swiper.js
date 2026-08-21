449ac8a99405bcf6f02e283b40ee77d54f681269
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-21 19:40:54 -0300

    proj016 adicionado

---
+++ proj016/swiper.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper(".burguer-swiper", {
+  loop: true,
+
+  slidesPerView: 3,
+
+  centeredSlides: true,
+
+  spaceBetween: 24,
+
+  speed: 900,
+
+  autoplay: {
+    delay: 2000,
+  },
+
+});
+
+const slideinfinite = new Swiper(".infinite-swiper", {
+  
+
+  slidesPerView: 4,
+
+  centeredSlides: true,
+
+  spaceBetween: 24,
+
+  speed: 2000,
+  
+  loop: true,
+
+  freeMode: true,
+
+  freeModeMomentum: false,
+
+  autoplay: {
+    delay: 0,
+
+    disableOnInteraction: false,
+  },
+
+});
